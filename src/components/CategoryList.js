d55983488a8f7f835b2807e98fd127f1efc05a30
Minna <minna.maaria.laitinen@gmail.com>
2019-10-14 01:16:30 +0300

    Simplify code for categories table

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,32 +1,19 @@
 import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 import '../css/CategoryList.css'
+import categories from '../assets/categories.json'
 
 const alertClicked = () => {
-  alert('You clicked the third ListGroupItem');
+  alert('Category clicked!');
 }
 
 export const CategoryList = () => (
 	<ListGroup variant="flush" style={{ flex: '2', border: '1px solid #DFDFDF', padding: '20px', marginRight: '40px' }}>
-		<h5 style={{ color: '#3C3C3C' }} >Categories</h5>
-  	<ListGroup.Item active>All</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}> Appetizers</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Beef</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Breads</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Budget friendly</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Casseroles</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Chicken</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Dinner</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Desserts</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Fish</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Healthy</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Kid friendly</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Pasta</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Pork</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Salads</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Slow cooker</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Snacks</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Soups</ListGroup.Item>
-		<ListGroup.Item action onClick={alertClicked}>Vegetable</ListGroup.Item>
+		<h5 style={{ color: '#413938' }} >Categories</h5>
+		{Object.values(categories).map(category => (
+			<ListGroup.Item key={category} action onClick={alertClicked}>
+				{category}
+			</ListGroup.Item>
+		))}
 	</ListGroup>
 )
