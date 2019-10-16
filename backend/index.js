949def69a4a9cb7df7dc99a34036883fd461fb1b
vchau <van.chau@aalto.fi>
2019-10-17 00:25:29 +0300

    test

--- backend/index.js
+++ backend/index.js
@@ -335,7 +335,7 @@ app.get('/api/recipes/:id', (req, res) => {
     }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
