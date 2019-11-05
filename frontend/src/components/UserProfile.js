337fa938c8416d5054645e503247da5802d9a05b
Minna Seppälä <mimsi.seppala@gmail.com>
2019-11-05 18:26:29 +0200

    tab titles jump when hovered, gotta fix

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,13 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
-import { Card } from 'react-bootstrap'
+import { Card, Tab, Tabs } from 'react-bootstrap'
 import '../css/UserProfile.css'
  
 
 const UserProfile = () => {
   const { username } = useParams()
-  const [user, setUser] = useState({ username: '', name: '', profilePicture: ''})
+	const [user, setUser] = useState({ username: '', name: '', profilePicture: ''})
+	const [activeTab, setActiveTab] = useState('1')
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,6 +21,9 @@ const UserProfile = () => {
 	
 	console.log(user)
 
+	const handleClick = (tab) => {
+			setActiveTab(tab)
+	}
  
   return (
     <React.Fragment>
@@ -28,6 +32,14 @@ const UserProfile = () => {
         <Card.Body>
 					<img className='profile-card-img rounded-circle' src={`.${user.profilePicture}`}></img>
 					<Card.Title className='profile-title'>{user.name}</Card.Title>
+					<Tabs className='profile-tabs' activeTab={1} onClick={handleClick}>
+						<Tab className='profile-tab' eventKey={1} title="Own Recipes">
+							tab content1
+						</Tab>
+						<Tab className='profile-tab' eventKey={2} title="Saved Recipes">
+							tab content2222
+						</Tab>
+					</Tabs>
         </Card.Body>
       </Card>
     </React.Fragment>
