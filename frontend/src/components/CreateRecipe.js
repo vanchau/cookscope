0655cbf3874011f46e3afc306f73e7d46068f5bb
Minna <minna.maaria.laitinen@gmail.com>
2019-11-05 15:58:48 +0200

    Add remove feature for ingredients

--- frontend/src/components/CreateRecipe.js
+++ frontend/src/components/CreateRecipe.js
@@ -2,22 +2,27 @@ import React, { useState } from 'react'
 import { Form, Button, Card } from 'react-bootstrap'
 
 import '../css/CreateRecipe.css'
+//import trash_can from '../assets/trash_can.png'
 
 const CreateRecipe = () => {
 
-  const [ingredientCount, addCount] 	= useState(1)
   const [ingredients, addIngredient]	= useState([{ name: '', id: 1 }])
   const [numberOfSteps, addStepCount]	= useState(1)
   const [steps, addInstruction] 			= useState([{ instruction: '', id: 1 }])
 
   const addNewIngredient = () => {
-    addCount(ingredientCount + 1)
-    addIngredient([ ...ingredients, { name: '', id: ingredientCount + 1 } ])
+    const newID = ingredients.length === 0 ? 1 : ingredients[ingredients.length - 1].id + 1
+    addIngredient([ ...ingredients, { name: '', id: newID } ])
   }
 
   const addStep = () => {
     addStepCount(numberOfSteps + 1)
     addInstruction([ ...steps, { instruction: '', id: numberOfSteps + 1 } ])
+  }
+
+  const removeIngredient = (id) => {
+    const newList = ingredients.filter(obj => obj.id !== id)
+    addIngredient(newList)
   }
 
   return (
@@ -27,7 +32,11 @@ const CreateRecipe = () => {
         <Card.Body>
 
           <Form.Group controlId='formGridAddress1'>
-            <Form.Control style={{ height: '80px', fontSize: '2rem', fontWeight: 'bold' }} type='text' placeholder='Enter recipe title' />
+            <Form.Control
+              style={{ height: '80px', fontSize: '2rem', fontWeight: 'bold' }}
+              type='text'
+              placeholder='Enter recipe title'
+            />
           </Form.Group>
 
           <Form.Group controlId='formGridAddress1'>
@@ -38,12 +47,14 @@ const CreateRecipe = () => {
           <hr style={{ marginTop: '0' }} />
 
           {ingredients.map(ingredient => (
-            <Form.Group key={ingredient.id} controlId={ingredient.id} className='ingredients'>
-              <Form.Control placeholder='2 cloves of garlic' />
+            <Form.Group key={ingredient.id} controlId={ingredient.id} className='ingredients' style={{ display: 'flex' }}>
+              <Form.Control placeholder='2 cloves of garlic' style={{ flex: '10'}}/>
+              <Button onClick={() => removeIngredient(ingredient.id)}>Remove</Button>
+              {/* <i style={{ flex: '1', marginLeft: '5px', maxHeight: '38px' }} src={trash_can} alt='logo' /> */}
             </Form.Group>
           ))}
 
-          <Button className='add-ingredient' onClick={addNewIngredient} >+</Button>
+          <Button className='add-ingredient-button' onClick={addNewIngredient} >add ingredient</Button>
 
           <h5 className='recipe-subtitles' >Instructions</h5>
           <hr style={{ marginTop: '0' }} />
