4dee9a60be1705a8c7c6271e1de0beffdb68a3e9
vchau <van.chau@aalto.fi>
2019-11-21 20:45:55 +0200

    add build for heroku

---
+++ build/precache-manifest.53ba1a5e677411a57f5e4176c0668407.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68d326e63302551582fab6b1ae51ba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "738002e3e4cae5ed9224",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "597d0081b83d8c4194fa",
+    "url": "/static/css/main.0468e466.chunk.css"
+  },
+  {
+    "revision": "738002e3e4cae5ed9224",
+    "url": "/static/js/2.5399ff2b.chunk.js"
+  },
+  {
+    "revision": "597d0081b83d8c4194fa",
+    "url": "/static/js/main.79502793.chunk.js"
+  },
+  {
+    "revision": "fcb2f0062004d41d0299",
+    "url": "/static/js/runtime-main.ede81beb.js"
+  },
+  {
+    "revision": "90a60c49524eb3de97291c859a1665cc",
+    "url": "/static/media/logo2.90a60c49.png"
+  },
+  {
+    "revision": "41afc6c2bc067054a93d154dade75bc6",
+    "url": "/static/media/subtle_white_feathers.41afc6c2.png"
+  }
+]);
