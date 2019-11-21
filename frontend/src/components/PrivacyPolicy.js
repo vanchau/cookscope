7bce79e31a460fcb472f16412cf3ac0da0f0af88
Minna <minna.maaria.laitinen@gmail.com>
2019-11-22 01:05:48 +0200

    Update terms and privacy policy

--- frontend/src/components/PrivacyPolicy.js
+++ frontend/src/components/PrivacyPolicy.js
@@ -1,81 +1,205 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-trailing-spaces */
 import React from 'react'
 
 const PrivacyPolicy = () => (
   <div style= {{backgroundColor: 'white', padding: '40px'}}>
     <h2>PRIVACY POLICY</h2>
-    <h4 style={{ paddingBottom: '40px' }} >Last Updated: September 15, 2019</h4>
+    <h4 style={{ paddingBottom: '40px' }} >Last Updated: November 15, 2019</h4>
     <h3>Contents</h3>
 
     <ul>
       <li>
-        <a className='link' href='#s1'>1. Overview</a>
+        <a className='link' href='#s1'>1. What does this Privacy Policy cover?</a>
       </li>
       <li>
-        <a className='link' href='#s2'>2. What does this Privacy Policy cover?</a>
+        <a className='link' href='#s2'>2. What Personal Information do we collect from you?</a>
       </li>
       <li>
-        <a className='link' href='#s3'>3. What Personal Information do we collect from you?</a>
+        <a className='link' href='#s3'>3. How do we use Personal Information collected from you?</a>
       </li>
       <li>
-        <a className='link' href='#s4'>4. Cookies</a>
+        <a className='link' href='#s4'>4. Who do we share your Personal Information with?</a>
       </li>
       <li>
-        <a className='link' href='#s5'>5. How do we use Personal Information collected from you?</a>
+        <a className='link' href='#s5'>5. How long do we keep your Personal Information?</a>
       </li>
       <li>
-        <a className='link' href='#s6'>6. Who do we share your Personal Information with?</a>
+        <a className='link' href='#s6'>6. How do we secure your Personal Information?</a>
       </li>
       <li>
-        <a className='link' href='#s7'>7. Our legal grounds for processing Personal Information</a>
+        <a className='link' href='#s7'>7. Changes to this Privacy Policy</a>
       </li>
       <li>
-        <a className='link' href='#s8'>8. Where is your Personal Information stored and processed?</a>
+        <a className='link' href='#s8'>8. Your rights</a>
       </li>
       <li>
-        <a className='link' href='#s9'>9. How long do we keep your Personal Information?</a>
-      </li>
-      <li>
-        <a className='link' href='#s10'>10. How do we secure your Personal Information?</a>
-      </li>
-      <li>
-        <a className='link' href='#s11'>11. Changes to this Privacy Policy</a>
-      </li>
-      <li>
-        <a className='link' href='#s12'>12. Your rights</a>
-      </li>
-      <li>
-        <a className='link' href='#s13'>13. Contacting us, further questions and how to make a complaint</a>
+        <a className='link' href='#s9'>9. Contacting us, further questions and how to make a complaint</a>
       </li>
     </ul>
 
-    <h2 id='#s1'>1. Overview</h2>
+    <h2 id='s1'>1. What does this Privacy Policy cover?</h2>
     <p>
-			This is the Website and App Privacy Policy (<b>&quot;Privacy Policy&quot;</b>) of the UK registered
-			branch of CookScope Inc., a corporation registered at 4-20-3 Ebisu, Shibuya-ku, Tokyo, Japan
-			(<b>&quot;CookScope&quot;</b>) and we are the data controller in respect of the Personal Information
-			(as defined below) that we collect from you.  It covers information CookScope collects from
-			users and visitors of our official website (<b>&quot;Site&quot;</b>) and official mobile app (<b>&quot;App&quot;</b>).
+      We process certain information about you visit or when you use our service provided by our Site
+      (&quot;CookScope Service&quot;). Some of that information may enable you to be identifiable.
     </p>
     <p>
-			By accessing our Site or App, you confirm that you have read and understood the entirety of
-			this Privacy Policy.
+      This Privacy Policy explains how we collect, store and use your Personal Information
+      when you visit or use our Site. It also describes the types of Personal Information we collect, 
+      how we use this Personal Information to ensure we provide a relevant and timely CookScope Service to you, 
+      and to improve your experience of the CookScope Service.
     </p>
-    <p>You have various rights in respect of our use of your Personal Information as set out in t
-			he <a className='link' href='#s13'>&apos;Your Rights&apos;</a> section below.  Two of the fundamental rights to be aware
-			of are that you may:
-    </p>
-    <ol type='1'>
+    <h2 id='s2'>2. What Personal Information do we collect from you?</h2>
+    <p>For registered users:</p>
+    <ul>
       <li>
-				ask us to stop using your Personal Information for direct-marketing by email or push
-				notification.  If you exercise this right, we will stop using your Personal Information
-				in this regard; and
+        Username, Email address and password.
       </li>
       <li>
-				ask us to consider any valid objections you have to our use of your Personal Information
-				where we process your Personal Information on the basis of our, or a third party&apos;s,
-				legitimate interests.
+        Recipe or food interests that you indicate through your use of the Site.
       </li>
-    </ol>
+      <li>
+        Personal Information you provide including your photo and username.
+      </li>
+      <li>
+        The CookScope Service can access and collect images stored in your device&apos;s photo storage.
+      </li>
+      <li>
+        Any Personal Information included in any of your recipes, comments or other submissions will be 
+        collected and processed by CookScope.
+      </li>
+    </ul>
+    <p>For all users:</p>
+    <ul>
+      <li>
+        Name, phone number or email address and any other Personal Information you provide to us in 
+        the course of our communication.
+      </li>
+      <li>
+        The internet protocol (IP) address used to connect your computer or device to the internet 
+        and geographical location determined from such;
+      </li>
+      <li>
+        Your login information, browser type and version, time zone setting, browser plug-in types and versions, 
+        device information such as operating system and platform (as applicable to the device you are using)
+      </li>
+      <li>
+        Information about your visit to our Site, the path that you take through and from our Site 
+        (including date and time); pages that you viewed or searched for; page response times, download errors, 
+        length of visits to certain pages, page interaction information (such as scrolling, clicks, and mouse-overs), 
+        and methods used to browse away from the page; and
+      </li>
+    </ul>
+    <h2 id='s3'>3. How do we use Personal Information collected from you?</h2>
+    <p>
+      We use the Personal Information we collect from you in the following ways: To provide, develop, maintain, 
+      tailor and improve the CookScope Service and any other services that we operate
+    </p>
+    <p>
+      For example, we might use your Personal Information to:
+    </p>
+    <ul>
+      <li>
+        verify your identity;
+      </li>
+      <li>
+        allow access to the Site;
+      </li>
+      <li>
+        facilitate the access and amendment of registration information;
+      </li>
+      <li>
+        ensure security of the Site.
+      </li>
+      <li>
+        promote safety and security on CookScope Service to protect you and your Personal Information;
+      </li>
+      <li>
+        deny use of the CookScope Service to previous registered users who have: (a) caused damage to CookScope, 
+        other users or third parties; or (b) breached our terms of use (&quot;Terms of Use&quot;) or used CookScope 
+        Service in a fraudulent or improper way; or
+      </li>
+      <li>
+        protect users of the CookScope Service and our Our Group against threats such as fraud or cyber-attacks.
+      </li>
+      <li>
+        to communicate with you in order to respond to queries you have raised with us.
+      </li>
+    </ul>
+    <h2 id='s4'>4. Who do we share your Personal Information with?</h2>
+    <p>
+      Other CookScope users:  We share information with other users of the CookScope Service. 
+    </p>
+    <p>
+      Beta testing users: If you join one of our beta testing groups then your information may be shared 
+      with other members of the testing group in order to enable communication and enhance the features being 
+      tested. People in our beta testing groups are able to leave these groups at any time.  
+    </p>
+    <h2 id='s5'>5. How long do we keep your Personal Information?</h2>
+    <p>
+      We will not keep your Personal Information longer than reasonably necessary to fulfil the purposes 
+      described in this Privacy Policy or as prescribed by law. In particular, we will implement a retention 
+      policy which stipulates the length of time we keep Personal Information. We will securely delete the Personal 
+      Information that we no longer need, and update, archive or securely delete Personal Information if it goes 
+      out of date. Therefore, we will retain any personal data related to your account for as long as you keep it open.
+    </p>
+    <p>
+      The length of time we retain Personal Information for depends on the purposes for which we collect and use it and/or 
+      as required to comply with applicable laws and to establish, exercise of defend our legal rights.
+    </p>
+    <h2 id='s6'>6. How do we secure your Personal Information?</h2>
+    <p>
+      We take steps in an effort to treat your information securely and in accordance with this Privacy Policy. 
+      Unfortunately though, the internet cannot be guaranteed to be 100% secure and, as such, you acknowledge and 
+      accept that we cannot guarantee the security of your Personal Information transmitted to the Site and that 
+      such transmission is at your own risk.
+    </p>
+    <p>
+      Where we have given you (or where you have chosen) a password which enables you to access your online account, 
+      you are responsible for keeping this password confidential. We ask you not to share a password with anyone.
+    </p>
+    <h2 id='s7'>7. Changes to this Privacy Policy</h2>
+    <p>
+      We may update this Privacy Policy from time to time. We will post any Privacy Policy changes on our Site. 
+      Depending on the significance of the changes, we may get in touch with you or display a more prominent notice. 
+      If we need to, we will e-mail you with details of those changes and/or ask you to confirm that you are happy 
+      with them. Please check back frequently to see any updates or changes to our Privacy Policy.
+    </p>
+    <h2 id='s8'>8. Your Rights</h2>
+    <p>
+      You have various rights in relation to your Personal Information. In particular, you have a right to:
+    </p>
+    <ul>
+      <li>
+        obtain confirmation that we are processing your Personal Information and request a copy of 
+        the Personal Information we hold about you;
+      </li>
+      <li>
+        ask that we update the Personal Information we hold about you, or correct such Personal Information 
+        that you think is incorrect or incomplete;
+      </li>
+      <li>
+        ask that we delete Personal Information that we hold about you, or restrict the way in which we use 
+        such Personal Information;
+      </li>
+      <li>
+        withdraw consent to our processing of your Personal Information, where we are relying upon your 
+        consent as our legal basis for processing such Personal Information;
+      </li>
+      <li>
+        object to our processing of your Personal Information;
+      </li>
+    </ul>
+    <p>To exercise any of your rights, please contact help@cookscope.com</p>
+    <h2 id='s9'>9. Contacting us, further questions and how to make a complaint</h2>
+    <p>
+      If you are unhappy with the way we handle your Personal Information, or any privacy query or request that you 
+      have raised with us, we would always prefer you to contact us first. However, you have a right to complain to the 
+      EU Data Protection Authority ("DPA") in the country where you usually live or work, or where an alleged infringement 
+      of privacy law has taken place. In the UK (for example), the DPA is the Information Commissioner's Office. 
+      If you would like to be directed to the appropriate DPA, please contact us at the contact details above. 
+      Alternatively, you may seek a remedy through the courts if you believe your rights have been breached.
+    </p>
   </div>
 )
 
--- frontend/src/components/Terms.js
+++ frontend/src/components/Terms.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-trailing-spaces */
 import React from 'react'
 
 import '../css/General.css'
@@ -5,7 +6,7 @@ import '../css/General.css'
 const TermsOfService = () => (
   <div style= {{backgroundColor: 'white', padding: '40px'}}>
     <h2>TERMS OF SERVICE</h2>
-    <h4 style={{ paddingBottom: '40px' }} >Last Updated: May 10, 2019</h4>
+    <h4 style={{ paddingBottom: '40px' }} >Last Updated: November 18, 2019</h4>
     <h3>Contents</h3>
 
     <ul>
@@ -16,32 +17,38 @@ const TermsOfService = () => (
         <a className='link' href='#s2'>2. Privacy</a>
       </li>
       <li>
-        <a className='link' href='#s3'>3. Content on the services</a>
+        <a className='link' href='#s3'>3. Information about us</a>
       </li>
       <li>
-        <a className='link' href='#s4'>4. Your rights and grant of rights in the content</a>
+        <a className='link' href='#s4'>4. Information about our site</a>
       </li>
       <li>
-        <a className='link' href='#s5'>5. Using the services</a>
+        <a className='link' href='#s5'>5. Important information regarding recipes</a>
       </li>
       <li>
-        <a className='link' href='#s6'>6. Your account</a>
+        <a className='link' href='#s6'>6. User registration</a>
       </li>
       <li>
-        <a className='link' href='#s7'>7. Your license to use the services</a>
+        <a className='link' href='#s7'>7. Your account and password</a>
       </li>
       <li>
-        <a className='link' href='#s8'>8. Ending these terms</a>
+        <a className='link' href='#s8'>8. Content owner and rights you license</a>
       </li>
       <li>
-        <a className='link' href='#s9'>9. Disclaimers and limitations of liability</a>
+        <a className='link' href='#s9'>9. Intellectual property rights</a>
       </li>
       <li>
-        <a className='link' href='#s10'>10. General</a>
+        <a className='link' href='#s10'>10. Code of conduct</a>
+      </li>
+      <li>
+        <a className='link' href='#s11'>11. Responsibility for content</a>
+      </li>
+      <li>
+        <a className='link' href='#s12'>12. Contact us</a>
       </li>
     </ul>
 
-    <h2 id='#s1'>1. Your agreement</h2>
+    <h2 id='s1'>1. Your agreement</h2>
     <p>
 			You may use the Services only if you agree to form a binding contract with CookScope and are
 			not a person barred from receiving services under the laws of the applicable jurisdiction.
@@ -51,14 +58,164 @@ const TermsOfService = () => (
 			to bind such entity to these Terms, in which case the words “you” and “your” as used in these
 			Terms shall refer to such entity.
     </p>
-    <h2 id='#s2'>2. Privacy</h2>
+    <h2 id='s2'>2. Privacy</h2>
     <p>
-		Our Privacy Policy (https://www.cookscope.com/privacy) describes how we handle the information
-		you provide to us when you use our Services. You understand that through your use of the Services
-		you consent to the collection and use (as set forth in the Privacy Policy) of this information,
-		including the transfer of this information to the United States, Ireland, and/or other countries
-		for storage, processing and use by Twitter and its affiliates.
+      Our Privacy Policy (https://cookscope.herokuapp.com/privacy) describes how we handle the information
+      you provide to us when you use our Services. You understand that through your use of the Services
+      you consent to the collection and use (as set forth in the Privacy Policy) of this information.
     </p>
+    <h2 id='s3'>3. Information about us</h2>
+    <p>
+      We are CookScope Inc., a corporation registered at xxxxxxxxx x x xx, Espoo, Finland.
+      If you have any question or complaint about our Site, or in any respect about CookScope, please contact us at: help@cookscope.com
+    </p>
+    <h2 id='s4'>4. Information about our site</h2>
+    <p>
+      Our Site provides you with an ability to create and share recipes with other users of our Site, and browse other users’ recipes. The service is free of charge, for the time being.
+      Our Site is designed to work on modern web browsers, major operating systems and platforms. Features and compatibility may vary between some web browsers, operating systems and platforms
+    </p>
+    <h2 id='s5'>5. Important information regarding recipes</h2>
+    <p>
+      All recipes on our Site are provided for general information only and published by other users, like you,
+      and not by CookScope. CookScope does not screen or check recipes before they are posted, so there may be some inappropriate,
+      incorrect or unsafe recipes, ingredients or cooking methods. The recipes, comments and other information provided on our
+      Site are not intended to amount to advice on which you should rely, and any use of or reliance on any recipe of
+      other content provided on our Site is at your own risk.
+    </p>
+    <p>
+      CookScope makes no statements, whether express or implied, about the accuracy, quality, safety or fitness for purpose
+      of recipes published on the CookScope Service.
+    </p>
+    <p>
+      Therefore:
+    </p>
+    <ul>
+      <li>
+        you should verify any unusual or novel ingredients or methods in a recipe by referring to other sources;
+      </li>
+      <li>
+        you should comply at all times with any food safety notices or bulletins issued by your local government authority,
+        and you should not expect the CookScope Service to note or explain risks contained in food preparation, method or techniques
+        of cooking or consumption of certain food; and
+      </li>
+      <li>
+        if you are not an experienced cook, we advise that you take instruction from an experienced cook,
+        or other appropriate authority, when using recipes from the CookScope Service, particularly in respect of health and safety.
+      </li>
+      <p>
+        If you have any comments or concerns about any recipes or other content on the CookScope Service or,
+        if you believe that your copyrighted work has been infringed or are aware of other infringing, illegal
+        or inappropriate material, please contact us at:
+      </p>
+    </ul>
+    <h2 id='s6'>6. User registration</h2>
+    <p>
+      With regard to our Site, you may visit and view the recipes without registration as a guest (“Guest”).
+      However, you will have access to more features of the CookScope Service if you create an account through
+      registration. If you wish to register for an account to use the CookScope Service, you will have to either
+      complete the required account information as requested by us through submitting the application in such
+      format as is specified by us.
+    </p>
+    <p>
+      We reserve the right to suspend, withdraw or discontinue your account, account details or password in the 
+      following circumstances. Where possible, we will give you notice of our intention to do this, and give you 
+      an opportunity to appeal:
+    </p>
+    <ul>
+      <li>
+        you do not exist;
+      </li>
+      <li>
+        you do not have an email address that could receive emails;
+      </li>
+      <li>
+        you breach these Terms of Service;
+      </li>
+      <li>
+        you have or appear to have intentionally provided false information to us;
+      </li>
+      <li>
+        to respond to actual or potential threats to our business (such as cyber-security threats), 
+        or unwelcome or dangerous practices or behaviours in our community; or
+      </li>
+    </ul>
+    <h2 id='s7'>7. Your account and password</h2>
+    <p>
+      If you choose to become a Registered User, you must treat your account information as confidential. 
+      You must not disclose it to any third party. You accept full responsibility and liability for all 
+      activities that occur under your account, except where it is our fault.
+    </p>
+    <h2 id='s8'>8. Content owner and rights you license</h2>
+    <p>
+      We do not claim ownership of any recipes, photographs, graphics, comments, information, data, text 
+      or other materials you upload or post on our Site.
+    </p>
+    <p>
+      However, when you upload Content, including a recipe, to our Site, you grant us a worldwide, royalty-free, 
+      on-going and transferable right and licence (which we may also grant to affiliates and a third party(ies), 
+      including other users) to use, copy, reproduce, process, adapt, modify, translate, publish, transmit, display 
+      and distribute (including via third parties) that Content or derivatives of it, in any and all media or 
+      distribution methods (now known or later developed) for any and all of our business activities.
+    </p>
+    <h2 id='s9'>9. Intellectual property rights</h2>
+    <p>
+      Except for your Content, including your recipes, all intellectual property rights in the CookScope Service 
+      and all materials on our Site, including design, graphics, text, pictures, software, other files and the 
+      selection and arrangement thereof are owned by us and/or our licensors.
+    </p>
+    <h2 id='s10'>10. Code of conduct</h2>
+    <p>
+      You promise that you will not:
+    </p>
+    <ol>
+      <li>
+        infringe intellectual property rights of third parties, including without limitation, 
+        intellectual property rights such as copyright and trademark, of us, other users or third parties;
+      </li>
+      <li>
+        infringe the property rights, privacy rights and image rights of us, other users or third parties;
+      </li>
+      <li>
+        disclose information, such as the name and email address of a specific individual without authorisation, 
+        and by which any third party is able to identify the said individual;
+      </li>
+      <li>
+        register multiple email addresses to obtain multiple accounts, while being a single user;
+      </li>
+      <li>
+        obtain an account on behalf of a Registered User whose account has been suspended or terminated;
+      </li>
+      <li>
+        discriminate against, harass, bully, intimidate, defame, or damage the reputation and credibility of, 
+        other users or third parties;
+      </li>
+      <li>
+        alter and/or erase Materials made available through the CookScope Service;
+      </li>
+      <li>
+        impersonate us, other users or third parties (including doctoring email headers for the purpose of 
+        falsifying the source of email headers and the identity of the sender);
+      </li>
+      <li>
+        transmit computer programmes or viruses that are harmful or cause other users or third parties 
+        to receive such programmes or viruses;
+      </li>
+      <li>
+        obtain account information without authorisation or through fraud (including, without limitation, 
+        by phishing or similar methods);
+      </li>
+      <li>
+        use the CookScope Service for election campaign or other political activities;
+      </li>
+    </ol>
+    <h2 id='s11'>11. Responsibility for content</h2>
+    <p>
+      You are solely responsible for the Content that you post, upload, and/or publish using the 
+      CookScope Service. We do not review or screen any of your Content before that Content appears or is shared.
+    </p>
+    <h2 id='s12'>12. Contact us</h2>
+    <p>To contact us, please email </p>
+    <a href='mailto:help@cookscope.com'>help@cookscope.com</a>
   </div>
 )
 
