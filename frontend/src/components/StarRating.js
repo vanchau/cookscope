c35c5d7c81582b8fd0cbf52c23fb17143443a152
Minna Seppälä <mimsi.seppala@gmail.com>
2019-11-21 18:03:08 +0200

    css fixes

--- frontend/src/components/StarRating.js
+++ frontend/src/components/StarRating.js
@@ -14,7 +14,7 @@ const StarRating = (props) => {
   return (
     <div className='row star-rating'>
       <ReactStars className='stars' color2='#FBB117' color1='LightGrey' edit={starEditing} half={starHalves} count={5} value={Math.round(rating*2)/2} onChange={submitRating}/>
-      <div className='rating'>{isNaN(rating) ? 0 : rating}</div>
+      <div className='rating'>{isNaN(rating) ? null : rating}</div>
     </div>
   )
 }
