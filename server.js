cbc19e2eee15034f2cf47fe8b6afc139a2482902
Minna Seppälä <mimsi.seppala@gmail.com>
2019-11-22 17:32:10 +0200

    modified search

--- server.js
+++ server.js
@@ -71,9 +71,7 @@ app.get('/api/recipes', async (request, response) => {
     } 
     else {
         const someMatch = recipes.filter(function(recipe){
-            let ingredientArray = recipe.ingredients.map(ingredient => ingredient.ingredient.split(' '))
-            ingredientArray = ingredientArray.flat()
-            console.log(ingredientArray)
+            let ingredientArray = recipe.ingredients.map(ingredient => ingredient.ingredient.split(' ')).flat()
 
             if (searchWords.some(word => recipe.title.toLowerCase().split(" ").includes(word))) return true
             if (searchWords.some(word => recipe.title.toLowerCase().includes(word))) return true
