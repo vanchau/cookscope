b66272be55f825d3edaee116196d230cc9b9bfc5
vchau <van.chau@aalto.fi>
2020-01-05 21:44:54 +0200

    add build

---
+++ build/precache-manifest.a7b7a96fb2d074a86787f465791fcea0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631f4592952c19a598e8565626b670f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b13b74870bbb4ff9c913",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ef62f157c5c3729fcd30",
+    "url": "/static/css/main.c5839036.chunk.css"
+  },
+  {
+    "revision": "b13b74870bbb4ff9c913",
+    "url": "/static/js/2.9908acdc.chunk.js"
+  },
+  {
+    "revision": "ef62f157c5c3729fcd30",
+    "url": "/static/js/main.1f094628.chunk.js"
+  },
+  {
+    "revision": "fcb2f0062004d41d0299",
+    "url": "/static/js/runtime-main.ede81beb.js"
+  },
+  {
+    "revision": "90a60c49524eb3de97291c859a1665cc",
+    "url": "/static/media/logo2.90a60c49.png"
+  },
+  {
+    "revision": "41afc6c2bc067054a93d154dade75bc6",
+    "url": "/static/media/subtle_white_feathers.41afc6c2.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c0da266d44fb1198ab4311446d376f4.js"
+  "/precache-manifest.a7b7a96fb2d074a86787f465791fcea0.js"
 );
 
 self.addEventListener('message', (event) => {
